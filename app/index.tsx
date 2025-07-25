21ed5d1a4c376766fb4d1e8638cec62b135eec72
sonkiee <annagu.kennedy@gmail.com>
2025-07-25 20:07:55 +0000

    Jul 25, 2025, 9:07 PM

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
 import Wrapper from "@/components/ui/Wrapper";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import PagerView from "react-native-pager-view";
 
-export default function Index() {
+const Index = () => {
   return (
     <Wrapper>
-      <Text> hhh</Text>
+      <PagerView style={styles.container} overdrag />
     </Wrapper>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+export default Index;
