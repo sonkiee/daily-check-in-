4ca3a622ddf771226952d019156e64050bab0ab3
sonkiee <annagu.kennedy@gmail.com>
2025-07-25 20:12:54 +0000

    Jul 25, 2025, 9:12 PM

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,5 @@
+import HomeScreen from "@/components/screens/Home";
+import SettingsScreen from "@/components/screens/Settings";
 import Wrapper from "@/components/ui/Wrapper";
 import React from "react";
 import { StyleSheet } from "react-native";
@@ -6,7 +8,10 @@ import PagerView from "react-native-pager-view";
 const Index = () => {
   return (
     <Wrapper>
-      <PagerView style={styles.container} overdrag />
+      <PagerView style={styles.container} initialPage={0} overdrag>
+        <HomeScreen key={1} />
+        <SettingsScreen key={2} />
+      </PagerView>
     </Wrapper>
   );
 };
