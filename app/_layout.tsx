07fb2619bfedee92edcca1f5cbec2e9cffb4df1d
sonkiee <annagu.kennedy@gmail.com>
2025-07-25 23:37:33 +0000

    Jul 26, 2025, 12:37 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import { IconSymbol } from "@/components/ui/IconSymbol";
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -23,9 +23,10 @@ const RootLayout = () => {
             headerShown: true,
             headerTransparent: true,
             headerTitle: "Home",
-
             headerRight: () => (
-              <TouchableOpacity onPress={() => console.log("Settings")}>
+              <TouchableOpacity
+                onPress={() => router.push("/(settings)/settings")}
+              >
                 <IconSymbol name="gearshape" color={"#333"} size={24} />
               </TouchableOpacity>
             ),
