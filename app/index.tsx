a39487ae4b49b53c707805df92cdd496cc4b37f0
sonkiee <annagu.kennedy@gmail.com>
2025-07-25 12:58:05 +0000

    Jul 25, 2025, 1:58 PM

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
 import Wrapper from "@/components/ui/Wrapper";
-import { Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-  const insets = useSafeAreaInsets();
   return (
     <Wrapper>
-      <View style={{ backgroundColor: "tranparent" }}>
-        <Text>Welcome to the Expo Router!</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hi Wanderer</Text>
       </View>
     </Wrapper>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
+  },
+});
