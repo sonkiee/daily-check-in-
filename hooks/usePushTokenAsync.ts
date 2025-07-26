216a52b31029b0f856fdddc78400aa401559ed90
sonkiee <annagu.kennedy@gmail.com>
2025-07-26 02:14:06 +0000

    Jul 26, 2025, 3:14 AM

---
+++ hooks/usePushTokenAsync.ts
@@ -0,0 +1,15 @@
+import { registerForPushNotificationsAsync } from "@/libs/notification";
+import { useEffect } from "react";
+
+const usePushTokenAync = () => {
+  useEffect(() => {
+    (async () => {
+      const token = await registerForPushNotificationsAsync();
+      if (token) {
+        console.log("Push Token:", token);
+      }
+    })();
+  });
+};
+
+export default usePushTokenAync;
