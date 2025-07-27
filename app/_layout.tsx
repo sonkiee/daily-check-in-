5ea16f9a542234407fd124341e13327a8455d14a
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 07:57:03 +0000

    Jul 27, 2025, 8:57 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { randomUUID } from "expo-crypto";
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { TouchableOpacity } from "react-native";
@@ -8,8 +9,9 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
-  console.log("First to log in ");
+  const uuid = randomUUID();
 
+  console.log("UUID", uuid);
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
