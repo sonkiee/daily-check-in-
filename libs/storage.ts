b9f114aa51d38057a0e62e9b2e42aff5dd2ff4d8
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 07:25:43 +0000

    Jul 27, 2025, 8:25 AM

--- libs/storage.ts
+++ libs/storage.ts
@@ -1,15 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Storage = {
-  set: async (key: string, value: string) => {
-    return await AsyncStorage.setItem(key, value);
-  },
-  get: async (key: string) => {
-    return await AsyncStorage.getItem(key);
-  },
-  remove: async (key: string) => {
-    return await AsyncStorage.removeItem(key);
-  },
+  set: (key: string, value: string) => AsyncStorage.setItem(key, value),
+  get: (key: string) => AsyncStorage.getItem(key),
+  remove: (key: string) => AsyncStorage.removeItem(key),
+  clear: () => AsyncStorage.clear(), // Optional utility
 };
 
 export default Storage;
