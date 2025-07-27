23acf60e6a9a283260728862aef9380209924a70
sonkiee <annagu.kennedy@gmail.com>
2025-07-27 07:15:09 +0000

    Jul 27, 2025, 8:15 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,44 +1,49 @@
 import { IconSymbol } from "@/components/ui/IconSymbol";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+const queryClient = new QueryClient();
+
 const RootLayout = () => {
   return (
     <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          animation: "fade",
-          animationMatchesGesture: true,
-          headerBackButtonDisplayMode: "minimal",
-          headerTintColor: "#111",
-          headerShadowVisible: false,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "Home",
-            headerShown: true,
-            headerTransparent: true,
-            headerTitle: "",
-            headerRight: () => (
-              <TouchableOpacity
-                onPress={() => router.push("/(settings)/settings")}
-              >
-                <IconSymbol name="gearshape" color={"#333"} size={24} />
-              </TouchableOpacity>
-            ),
+      <QueryClientProvider client={queryClient}>
+        <Stack
+          screenOptions={{
+            animation: "fade",
+            animationMatchesGesture: true,
+            headerBackButtonDisplayMode: "minimal",
+            headerTintColor: "#111",
+            headerShadowVisible: false,
           }}
-        />
-        <Stack.Screen
-          name="(settings)"
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Stack>
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              title: "Home",
+              headerShown: true,
+              headerTransparent: true,
+              headerTitle: "",
+              headerRight: () => (
+                <TouchableOpacity
+                  onPress={() => router.push("/(settings)/settings")}
+                >
+                  <IconSymbol name="gearshape" color={"#333"} size={24} />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="(settings)"
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack>
+      </QueryClientProvider>
       <StatusBar style="dark" />
     </SafeAreaProvider>
   );
