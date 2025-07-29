d99233f10b264c8e3165bb77317ae32886cd0486
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 02:42:53 +0000

    Jul 29, 2025, 3:42 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,7 @@
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/react-query";
 import { randomUUID } from "expo-crypto";
-import { router, Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Redirect } from "expo-router";
 import { useState } from "react";
-import { Alert, TouchableOpacity } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
 
@@ -13,50 +9,51 @@ const RootLayout = () => {
   const [newDevice, setNewDevice] = useState(true);
   const uuid = randomUUID();
 
-  if (newDevice) {
-    Alert.alert("Welcome", "You're most welcome to our app new tome usrr");
-  }
+  // if (newDevice) {
+  //   Alert.alert("Welcome", "You're most welcome to our app new tome usrr");
+  // }
 
-  console.log("UUID", uuid);
-  return (
-    <SafeAreaProvider>
-      <QueryClientProvider client={queryClient}>
-        <Stack
-          screenOptions={{
-            animation: "fade",
-            animationMatchesGesture: true,
-            headerBackButtonDisplayMode: "minimal",
-            headerTintColor: "#111",
-            headerShadowVisible: false,
-          }}
-        >
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Home",
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: () => (
-                <TouchableOpacity
-                  onPress={() => router.push("/(settings)/settings")}
-                >
-                  <IconSymbol name="gearshape" color={"#333"} size={24} />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="(settings)"
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack>
-      </QueryClientProvider>
-      <StatusBar style="dark" />
-    </SafeAreaProvider>
-  );
+  return <Redirect href={"/(auth)"} />;
+
+  // return (
+  //   <SafeAreaProvider>
+  //     <QueryClientProvider client={queryClient}>
+  //       <Stack
+  //         screenOptions={{
+  //           animation: "fade",
+  //           animationMatchesGesture: true,
+  //           headerBackButtonDisplayMode: "minimal",
+  //           headerTintColor: "#111",
+  //           headerShadowVisible: false,
+  //         }}
+  //       >
+  //         <Stack.Screen
+  //           name="index"
+  //           options={{
+  //             title: "Home",
+  //             headerShown: true,
+  //             headerTransparent: true,
+  //             headerTitle: "",
+  //             headerRight: () => (
+  //               <TouchableOpacity
+  //                 onPress={() => router.push("/(settings)/settings")}
+  //               >
+  //                 <IconSymbol name="gearshape" color={"#333"} size={24} />
+  //               </TouchableOpacity>
+  //             ),
+  //           }}
+  //         />
+  //         <Stack.Screen
+  //           name="(settings)"
+  //           options={{
+  //             headerShown: false,
+  //           }}
+  //         />
+  //       </Stack>
+  //     </QueryClientProvider>
+  //     <StatusBar style="dark" />
+  //   </SafeAreaProvider>
+  // );
 };
 
 export default RootLayout;
