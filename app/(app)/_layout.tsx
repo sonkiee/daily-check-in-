cbb23f8a0058284a5067abd53822becf241acad9
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:27:25 +0000

    Jul 29, 2025, 4:27 AM

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 
 const AppLayout = () => {
   return (
-    <Stack>
+    <Stack initialRouteName="index">
       <Stack.Screen name="index" />
       <Stack.Screen name="(settings)" />
     </Stack>
