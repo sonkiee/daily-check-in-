4b4fa9d4d877d1478df39e5398f9508ab950ddd8
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:14:35 +0000

    Jul 29, 2025, 4:14 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,6 @@
-import { IconSymbol } from "@/components/ui/IconSymbol";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { router, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
@@ -11,47 +9,9 @@ const RootLayout = () => {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
-        <Stack
-          screenOptions={
-            {
-              // animation: "fade",
-              // animationMatchesGesture: true,
-              // headerBackButtonDisplayMode: "minimal",
-              // headerTintColor: "#111",
-              // headerShadowVisible: false,
-            }
-          }
-        >
-          <Stack.Screen
-            name="index"
-            options={{
-              title: "Home",
-              headerShown: true,
-              headerTransparent: true,
-              headerTitle: "",
-              headerRight: () => (
-                <TouchableOpacity
-                  onPress={() => router.push("/(settings)/settings")}
-                >
-                  <IconSymbol name="gearshape" color={"#333"} size={24} />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="(settings)"
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="(auth)"
-            options={{
-              headerShown: false,
-              presentation: "formSheet",
-            }}
-          />
-        </Stack>
+        <Stack.Screen name="(app)" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(onboarding)" />
       </QueryClientProvider>
       <StatusBar style="dark" />
     </SafeAreaProvider>
