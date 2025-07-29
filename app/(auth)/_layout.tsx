bdbc115c5a68be1c0ea4de3526b8f0aac610d5ae
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 04:31:57 +0000

    Jul 29, 2025, 5:31 AM

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from "expo-router";
 
 const AuthLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        presentation: "formSheet", // clean, lean, iOS native feel
+        animation: "slide_from_bottom", // optional: nice on Android
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
-          presentation: "formSheet", // clean, lean, iOS native feel
-          animation: "slide_from_bottom", // optional: nice on Android
-          headerShown: false, // remove clutter
+          title: "Auth",
         }}
       />
     </Stack>
