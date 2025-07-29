5db5b08bc6697a2b9e89d7729a9cb17fcbc960e7
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:45:47 +0000

    Jul 29, 2025, 4:45 AM

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -7,9 +7,9 @@ import PagerView from "react-native-pager-view";
 const IndexScreen = () => {
   return (
     <Wrapper>
-      <PagerView style={styles.container} initialPage={1} overdrag>
-        <HomeScreen key={1} />
-        <ClaimScreen key={2} />
+      <PagerView style={styles.container} initialPage={0} overdrag>
+        <HomeScreen key={0} />
+        <ClaimScreen key={1} />
       </PagerView>
     </Wrapper>
   );
