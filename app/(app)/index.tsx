162919a389234ecacff42bb375ca54d4bb4a0d1f
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:09:30 +0000

    Jul 29, 2025, 4:09 AM

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import ClaimScreen from "@/components/screens/Claim";
+import Wrapper from "@/components/ui/Wrapper";
+import { StyleSheet } from "react-native";
+import PagerView from "react-native-pager-view";
 
 const HomeScreen = () => {
-  return <Text></Text>;
+  return (
+    <Wrapper>
+      <PagerView style={styles.container} initialPage={0} overdrag>
+        <HomeScreen key={1} />
+        <ClaimScreen key={2} />
+      </PagerView>
+    </Wrapper>
+  );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
