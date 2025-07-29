52195bdd4fb98dcf52f7adc01a12d2edf4a03ce3
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:35:00 +0000

    Jul 29, 2025, 4:35 AM

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,10 +1,32 @@
-import { Stack } from "expo-router";
+import { IconSymbol } from "@/components/ui/IconSymbol.ios";
+import { router, Stack } from "expo-router";
+import { TouchableOpacity } from "react-native";
 
 const AppLayout = () => {
   return (
     <Stack initialRouteName="index">
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(settings)" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: true,
+          headerTransparent: true,
+          headerTitle: "",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => router.push("/(app)/(settings)/settings")}
+            >
+              <IconSymbol name="gearshape" color={"#333"} size={24} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="(settings)"
+        options={{
+          headerShown: false,
+        }}
+      />
     </Stack>
   );
 };
