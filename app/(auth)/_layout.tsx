4cfe9a80066a8f52fc5c9b170ce0fd6b595b70d1
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 04:28:42 +0000

    Jul 29, 2025, 5:28 AM

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,7 +3,12 @@ import { Stack } from "expo-router";
 const AuthLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{}} />
+      <Stack.Screen
+        name="index"
+        options={{
+          presentation: "formSheet",
+        }}
+      />
     </Stack>
   );
 };
