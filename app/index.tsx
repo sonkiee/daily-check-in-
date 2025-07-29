8fd20b10c6a8a84f9622ca6736012b880acc6492
sonkiee <annagu.kennedy@gmail.com>
2025-07-29 03:12:38 +0000

    Jul 29, 2025, 4:12 AM

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,15 @@
-import ClaimScreen from "@/components/screens/Claim";
-import HomeScreen from "@/components/screens/Home";
-import Wrapper from "@/components/ui/Wrapper";
-import usePushNotifications from "@/hooks/usePushNotifications";
+import { Redirect } from "expo-router";
 import React from "react";
-import { StyleSheet } from "react-native";
-import PagerView from "react-native-pager-view";
 
 const Index = () => {
-  const token = usePushNotifications();
-  console.log("Expo Push Token:", token);
+  // const token = usePushNotifications();
+  // console.log("Expo Push Token:", token);
 
   // if (newDevice) {
   //   Alert.alert("Welcome", "You're most welcome to our app new tome usrr");
   // }
 
-  return (
-    <Wrapper>
-      <PagerView style={styles.container} initialPage={0} overdrag>
-        <HomeScreen key={1} />
-        <ClaimScreen key={2} />
-      </PagerView>
-    </Wrapper>
-  );
+  return <Redirect href="/(app)" />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default Index;
