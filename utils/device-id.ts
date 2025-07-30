1ec41788b37e16a64e6107a2e173e423277cf9da
sonkiee <annagu.kennedy@gmail.com>
2025-07-30 18:03:07 +0000

    Jul 30, 2025, 7:03 PM

---
+++ utils/device-id.ts
@@ -0,0 +1,13 @@
+import * as Crypto from "expo-crypto";
+import * as SecureStore from "expo-secure-store";
+
+const DEVICE_ID_KEY = "deviceId";
+
+export const genDeviceId = async () => {
+  let id = await SecureStore.getItemAsync(DEVICE_ID_KEY);
+  if (!id) {
+    id = Crypto.randomUUID();
+    await SecureStore.setItemAsync(DEVICE_ID_KEY, id);
+  }
+  return { id };
+};
