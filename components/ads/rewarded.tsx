7dc947147ae557d6b4fb90c1780b88cc0e5381a1
sonkiee <annagu.kennedy@gmail.com>
2025-08-07 00:45:23 +0000

    Aug 7, 2025, 1:45 AM

--- components/ads/rewarded.tsx
+++ components/ads/rewarded.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { View } from "react-native";
+import {
+  RewardedAd,
+  RewardedAdEventType,
+  TestIds,
+} from "react-native-google-mobile-ads";
 
+const adUnitId = __DEV__
+  ? TestIds.REWARDED
+  : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
+
+const rewarded = RewardedAd.createForAdRequest(adUnitId, {});
 const RewardedAds = () => {
+  const [loaded, setLoaded] = useState();
+
+  useEffect(() => {
+    const unsubscribeLoaded = rewarded.addAdEventsListener(
+      RewardedAdEventType.LOADED,
+      () => {
+        setLoaded(true);
+      }
+    );
+  }, []);
   return <View></View>;
 };
 
