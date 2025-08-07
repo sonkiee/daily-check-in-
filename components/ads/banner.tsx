46506d8ea9250b4d4e48f4b36e3df421d315e3ec
sonkiee <annagu.kennedy@gmail.com>
2025-08-07 00:23:45 +0000

    Aug 7, 2025, 1:23 AM

--- components/ads/banner.tsx
+++ components/ads/banner.tsx
@@ -1,7 +1,12 @@
-import { View } from "react-native";
+import React from "react";
+import { BannerAd, TestIds } from "react-native-google-mobile-ads";
+
+const adUnitId = __DEV__
+  ? TestIds.ADAPTIVE_BANNER
+  : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
 
 const BannerAds = () => {
-  return <View></View>;
+  return <BannerAd unitId={adUnitId} />;
 };
 
 export default BannerAds;
