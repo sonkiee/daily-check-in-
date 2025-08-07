cd3cb48526af6549f0a216c01057b65432b6dc1a
sonkiee <annagu.kennedy@gmail.com>
2025-08-07 00:24:45 +0000

    Aug 7, 2025, 1:24 AM

--- components/ads/banner.tsx
+++ components/ads/banner.tsx
@@ -1,12 +1,26 @@
 import React from "react";
-import { BannerAd, TestIds } from "react-native-google-mobile-ads";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+} from "react-native-google-mobile-ads";
 
 const adUnitId = __DEV__
   ? TestIds.ADAPTIVE_BANNER
   : "ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy";
 
 const BannerAds = () => {
-  return <BannerAd unitId={adUnitId} />;
+  return (
+    <BannerAd
+      unitId={adUnitId}
+      size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      requestOptions={{
+        networkExtras: {
+          collapsible: "bottom",
+        },
+      }}
+    />
+  );
 };
 
 export default BannerAds;
