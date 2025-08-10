5921c30fec0ccdb6f379557d8b7a8a95b53a30c4
sonkiee <annagu.kennedy@gmail.com>
2025-08-10 01:07:28 +0000

    Aug 10, 2025, 2:07 AM

--- config/admob.ts
+++ config/admob.ts
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 
 export const admobConfig = {
   banner: Platform.select({
-    ios: "",
+    ios: "ca-app-pub-2231106094660297/4676890765",
     android: "",
   }),
   interstitial: Platform.select({
