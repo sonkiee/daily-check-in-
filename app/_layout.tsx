7f26fae466fd407453a525a6423afae96067addf
sonkiee <annagu.kennedy@gmail.com>
2025-08-13 02:30:24 +0000

    Aug 13, 2025, 3:30 AM

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,20 @@ import usePushTokenAync from "@/hooks/usePushTokenAsync";
 import { QueryClient } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import mobileAds from "react-native-google-mobile-ads";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
 
 const RootLayout = () => {
   const pushToken = usePushTokenAync();
+
+  mobileAds()
+    .initialize()
+    .then((adapterStatuses) => {
+      console.log("Admob initialized", adapterStatuses);
+    });
+
   return (
     <>
       <SafeAreaProvider>
